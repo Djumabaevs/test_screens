468c1daaa1bd66e393e3bba640054b5ea1fbc3ca
BakytDzhumabaev <djumabaevb@gmail.com>
2021-12-05 09:31:52 +0600

    failed to cancel null safety

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +12,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Mancho',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeCalendarPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+class HomeCalendarPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeCalendarPageState createState() => _HomeCalendarPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomeCalendarPageState extends State<HomeCalendarPage> {
+  CalendarController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 376,
-        height: 158,
-        child: Stack(children: <Widget>[
-          Positioned(
-              top: 0,
-              left: 0,
-              child: Container(
-                  width: 376,
-                  height: 158,
-                  decoration: BoxDecoration(),
-                  child: Stack(children: <Widget>[
-                    Positioned(
-                        top: 0,
-                        left: 0,
-                        child: Container(
-                            width: 376,
-                            height: 158,
-                            decoration: BoxDecoration(
-                              color: Color.fromRGBO(255, 255, 255, 1),
-                            ))),
-                    Positioned(
-                        top: 157,
-                        left: 0,
-                        child: //Mask holder Template
-                            Container(width: 376, height: 1, child: null)),
-                  ]))),
-          Positioned(
-              top: 46,
-              left: 1,
-              child: Container(
-                  width: 375,
-                  height: 56,
-                  decoration: BoxDecoration(),
-                  child: Stack(children: <Widget>[
-                    Positioned(
-                        top: 28,
-                        left: 88,
-                        child: Text(
-                          '9 января 2020 г. 14:00',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              color: Color.fromRGBO(0, 0, 0, 1),
-                              fontFamily: 'Open Sans',
-                              fontSize: 18,
-                              letterSpacing: 0,
-                              fontWeight: FontWeight.normal,
-                              height: 1.3333333333333333),
-                        )),
-                    Positioned(
-                        top: 0,
-                        left: 0,
-                        child: Container(
-                            width: 375,
-                            height: 80,
-                            decoration: BoxDecoration(
-                              color: Color.fromRGBO(255, 255, 255, 1),
-                            ))),
-                    Positioned(
-                        top: 16.00006103515625,
-                        left: 16,
-                        child: //Mask holder Template
-                            Container(width: 24, height: 24, child: null)),
-                    Positioned(
-                        top: 16,
-                        left: 64,
-                        child: Text(
-                          'Выберите дату и время',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              color: Color.fromRGBO(34, 43, 69, 1),
-                              fontFamily: 'Open Sans',
-                              fontSize: 18,
-                              letterSpacing: 0,
-                              fontWeight: FontWeight.normal,
-                              height: 1.3333333333333333),
-                        )),
-                  ]))),
-        ]));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Calendar Example'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TableCalendar(
+              initialCalendarFormat: CalendarFormat.month,
+              calendarStyle: CalendarStyle(
+                  todayColor: Colors.blue,
+                  selectedColor: Theme.of(context).primaryColor,
+                  todayStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0,
+                      color: Colors.white)),
+              headerStyle: HeaderStyle(
+                centerHeaderTitle: true,
+                formatButtonDecoration: BoxDecoration(
+                  color: Colors.brown,
+                  borderRadius: BorderRadius.circular(22.0),
+                ),
+                formatButtonTextStyle: TextStyle(color: Colors.white),
+                formatButtonShowsNext: false,
+              ),
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              // onDaySelected: (date, events) {
+
+              // },
+              builders: CalendarBuilders(
+                selectedDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(5.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    )),
+                todayDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(5.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    )),
+              ),
+              calendarController: _controller,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
